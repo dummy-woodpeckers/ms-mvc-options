1fed82c70fd61a1df94013606da9b4d9d28cb40a
Serhii Kuzmychov <ku3mich@gmail.com>
2021-06-09 13:24:04 +0300

    options examples

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ms_mvc_options.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly Settings Settings;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IOptionsSnapshot<Settings> settings)
+        {
+            _logger = logger;
+            Settings = settings.Value;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            var items = Settings.Summaries.Items;
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = items[rng.Next(items.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
